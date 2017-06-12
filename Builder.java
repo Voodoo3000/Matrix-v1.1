7ca334008ebc6cf25c902e091e4a5736db8fb399
Voodoo3000 <m047wbm@mail.ru>
2017-06-12 12:29:34 +0600

    Add files via upload

---
+++ Builder.java
@@ -0,0 +1,8 @@
+package com.voodoo.dtstr.api;
+
+/**
+ * Created by Voodoo3000 on 10.06.2017.
+ */
+public interface Builder {
+    int[][] build();
+}
---
+++ Calculator.java
@@ -0,0 +1,8 @@
+package com.voodoo.dtstr.api;
+
+/**
+ * Created by Voodoo3000 on 11.06.2017.
+ */
+public interface Calculator {
+    void multiply(int[][] matrixA, int[][] matrixB);
+}
